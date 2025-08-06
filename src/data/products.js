682904cc66d0554e7ad06f1e89647177928ff8fb
Kevin Rispoli <kevin.rispoli@yahoo.com>
2025-08-06 15:14:09 -0400

    Update products.js
    changes

--- src/data/products.js
+++ src/data/products.js
@@ -1,8 +1,8 @@
 [
-  { "id": 1, "name": "T-shirt",  "price": 19.99, "image": "/images/tshirt.png" },
-  { "id": 2, "name": "Jeans",    "price": 49.99, "image": "/images/jeans.png" },
-  { "id": 3, "name": "Sneakers", "price": 89.99, "image": "/images/sneakers.png" },
-  { "id": 4, "name": "Hat",      "price": 14.99, "image": "/images/hat.png" },
-  { "id": 5, "name": "Jacket",   "price": 79.99, "image": "/images/jacket.png" },
-  { "id": 6, "name": "Watch",    "price": 129.99,"image": "/images/watch.png" }
+  { "id": 1, "name": "T-shirt",  "price": 19.99, "image": "public/images/tshirt.png" },
+  { "id": 2, "name": "Jeans",    "price": 49.99, "image": "public/images/jeans.png" },
+  { "id": 3, "name": "Sneakers", "price": 89.99, "image": "public/images/sneakers.png" },
+  { "id": 4, "name": "Hat",      "price": 14.99, "image": "public/images/hat.png" },
+  { "id": 5, "name": "Jacket",   "price": 79.99, "image": "public/images/jacket.png" },
+  { "id": 6, "name": "Watch",    "price": 129.99,"image": "public/images/watch.png" }
 ]
