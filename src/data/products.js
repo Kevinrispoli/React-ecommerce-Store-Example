8620034ecb5ba2055e70da368c6419f8f5aa764e
Kevin Rispoli <kevin.rispoli@yahoo.com>
2025-08-08 20:19:30 -0400

    Update products.js
    yes

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,16 @@
-// Export an array of product objects pointing at your public/images folder
+// src/data/products.js
+import tshirt   from '../assets/images/tshirt.png';
+import jeans    from '../assets/images/jeans.png';
+import sneakers from '../assets/images/sneakers.png';
+import hat      from '../assets/images/hat.png';
+import jacket   from '../assets/images/jacket.png';
+import watch    from '../assets/images/watch.png';
+
 const products = [
-  { id: 1, name: 'T-shirt',  price: 19.99, image: '/images/tshirt.png' },
-  { id: 2, name: 'Jeans',    price: 49.99, image: '/images/jeans.png' },
-  { id: 3, name: 'Sneakers', price: 89.99, image: '/images/sneakers.png' },
-  { id: 4, name: 'Hat',      price: 14.99, image: '/images/hat.png' },
-  { id: 5, name: 'Jacket',   price: 79.99, image: '/images/jacket.png' },
-  { id: 6, name: 'Watch',    price: 129.99, image: '/images/watch.png' }
+  { id: 1, name: 'T-shirt',  price: 19.99, image: tshirt },
+  { id: 2, name: 'Jeans',    price: 49.99, image: jeans },
+  { id: 3, name: 'Sneakers', price: 89.99, image: sneakers },
+  { id: 4, name: 'Hat',      price: 14.99, image: hat },
+  { id: 5, name: 'Jacket',   price: 79.99, image: jacket },
+  { id: 6, name: 'Watch',    price:129.99, image: watch },
 ];
-
-export default products;
-
